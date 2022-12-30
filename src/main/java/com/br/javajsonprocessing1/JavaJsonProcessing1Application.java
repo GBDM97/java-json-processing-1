12df1d0787145c487eb57d9a83043b169346ab65
GBDM97 <ghian.m@gmail.com>
2022-12-30 15:11:56 -0300

    selected first object key

--- src/main/java/com/br/javajsonprocessing1/JavaJsonProcessing1Application.java
+++ src/main/java/com/br/javajsonprocessing1/JavaJsonProcessing1Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.br.javajsonprocessing1.AnnotationsTest.Serv;
 
 import org.json.simple.parser.ParseException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -22,13 +24,15 @@ public class JavaJsonProcessing1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaJsonProcessing1Application.class, args);
-	// 	try {
-	// 		JSONParser parser = new JSONParser();
-	// 		File file = new File("src/data.json");
-	// 		Object obj = parser.parse(new FileReader(file));
-	// 		System.out.println("=======>" + obj);
-	// 	} catch (Exception e) {
-	// 		System.out.println("=======>" + e);
-	// 	}
-	// }
-}}
+		try {
+			JSONParser parser = new JSONParser();
+			File file = new File("src/data.json");
+			Object obj = parser.parse(new FileReader(file));
+			JSONObject js0 = (JSONObject) obj;
+			var js = js0.get("world");
+			System.out.println("=======>" + js);
+		} catch (Exception e) {
+			System.out.println("=======>" + e);
+		}
+	}
+}
