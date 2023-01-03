c7f85f7ba46205ba7d76426c93cb58d4b59c2a6a
GBDM97 <ghian.m@gmail.com>
2023-01-02 21:14:40 -0300

    trying to find the first key in the Json String

--- src/main/java/com/br/javajsonprocessing1/JavaJsonProcessing1Application.java
+++ src/main/java/com/br/javajsonprocessing1/JavaJsonProcessing1Application.java
@@ -6,17 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.br.javajsonprocessing1.AnnotationsTest.Serv;
 
-import org.json.simple.parser.ParseException;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 import java.io.File;
-import java.io.FileReader;
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @SpringBootApplication
@@ -24,29 +17,31 @@ public class JavaJsonProcessing1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaJsonProcessing1Application.class, args);
+		String s1 = jParse.jParser();
 		try {
-			JSONParser parser = new JSONParser();
-			File file = new File("src/main/java/com/br/json/data.json");
-			Object obj = parser.parse(new FileReader(file));
-			JSONObject js0 = (JSONObject) obj;
-			JSONObject js1 = (JSONObject) js0.get("world");
-			JSONArray js2 = (JSONArray) js1.get("calvaryFriends");
-			JSONObject js3 = (JSONObject) js2.get(3);
-			JSONArray js4 = (JSONArray) js3.get("otherInfo");
-			JSONObject js5 = (JSONObject) js4.get(0);
-			JSONArray js6 = (JSONArray) js5.get("brother");
-			JSONObject js7 = (JSONObject) js6.get(0);
-			String js8 = (String) js7.get("country");
-		
+			for (int fi = initFi(); fi < s1.length();) {
+				if (s1.substring(fi, fi + 1) == ","|| s1.substring(fi, fi + 1) == "[" || s1.substring(fi, fi + 1) == "{"){
+					fi = s1.indexOf("\"", fi);
+					int ei = s1.indexOf("\"", fi + 1);
+					String out = s1.substring(fi, ei);
+					System.out.println(out);
+				}
 
-			// JSONArray js3 = (JSONArray) js2.get("Aaron");
-			// String js3 = (String) js1.get("name");
-
-
-
-			System.out.println("=======>" + js8);
+			}	
 		} catch (Exception e) {
 			System.out.println("=======>" + e);
 		}
+	}
+
+	private static int initFi() {
+		String s1 = jParse.jParser();
+		if (s1.indexOf("{") > s1.indexOf("[")){
+			int fi = s1.indexOf("{");
+			return fi;
+		} else {
+			int fi = s1.indexOf("[");
+			return fi;
+		}
+		
 	}
 }
--- src/main/java/com/br/javajsonprocessing1/jParse.java
+++ src/main/java/com/br/javajsonprocessing1/jParse.java
@@ -2,23 +2,34 @@ package com.br.javajsonprocessing1;
 
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import java.io.File;
-import java.io.FileReader;
-import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-public class jParse {{
+public class jParse {
 
+public static String jParser() {
+    
     try {
         JSONParser parser = new JSONParser();
-        File file = new File("data.json");
-        Object obj = parser.parse(new FileReader(file));
-        System.out.println("=======>" + obj);
+		File file = new File("src/main/java/com/br/json/data.json");
+		Object obj = parser.parse(new FileReader(file));
+		JSONObject js0 = (JSONObject) obj;
+		JSONObject js1 = (JSONObject) js0.get("world");
+		JSONArray js2 = (JSONArray) js1.get("calvaryFriends");
+		JSONObject js3 = (JSONObject) js2.get(3);
+		JSONArray js4 = (JSONArray) js3.get("otherInfo");
+		var s1 = js4.toString();
+        return s1;
     } catch (Exception e) {
         System.out.println("=======>" + e);
+        return " ";
     }
+}
     
-}}
+}
